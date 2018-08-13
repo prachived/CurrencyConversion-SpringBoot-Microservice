55189abb51239b959615e7a609cda393062c709b
prachived <prachived26@gmail.com>
2018-08-13 19:19:15 -0400

    Incorporated Feign proxy

--- src/main/java/com/currnecyconversion/microservice/springbootcurrencyconversionmicroservice/CurrencyConversionController.java
+++ src/main/java/com/currnecyconversion/microservice/springbootcurrencyconversionmicroservice/CurrencyConversionController.java
@@ -1,9 +1,13 @@
 package com.currnecyconversion.microservice.springbootcurrencyconversionmicroservice;
 
 import java.math.BigDecimal;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +17,10 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+  @Autowired
+  private CurrencyExchangeServiceProxy proxy;
 
   @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -32,4 +39,17 @@ public class CurrencyConversionController {
     return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
         quantity.multiply(response.getConversionMultiple()), response.getPort());
   }
+
+  @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+  public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+      @PathVariable BigDecimal quantity) {
+
+    CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+    logger.info("{}", response);
+
+    return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+        quantity.multiply(response.getConversionMultiple()), response.getPort());
+  }
+
 }
--- src/main/java/com/currnecyconversion/microservice/springbootcurrencyconversionmicroservice/SpringbootCurrencyconversionMicroserviceApplication.java
+++ src/main/java/com/currnecyconversion/microservice/springbootcurrencyconversionmicroservice/SpringbootCurrencyconversionMicroserviceApplication.java
@@ -1,12 +1,14 @@
 package com.currnecyconversion.microservice.springbootcurrencyconversionmicroservice;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
+@org.springframework.cloud.openfeign.EnableFeignClients("com.currnecyconversion.microservice.springbootcurrencyconversionmicroservice")
 public class SpringbootCurrencyconversionMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCurrencyconversionMicroserviceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootCurrencyconversionMicroserviceApplication.class, args);
+  }
 }
