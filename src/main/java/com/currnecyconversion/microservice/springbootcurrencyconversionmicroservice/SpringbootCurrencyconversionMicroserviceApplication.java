ca024c32a104da7d24901f5827ae805ad0a03864
prachived <prachived26@gmail.com>
2018-08-13 19:08:15 -0400

    Initial commit

---
+++ src/main/java/com/currnecyconversion/microservice/springbootcurrencyconversionmicroservice/SpringbootCurrencyconversionMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.currnecyconversion.microservice.springbootcurrencyconversionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCurrencyconversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCurrencyconversionMicroserviceApplication.class, args);
+	}
+}
